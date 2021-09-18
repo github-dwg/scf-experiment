691bd6fb49732187e8386ff625119cc57853c4e7
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-09-18 09:29:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --single-branch --branch main https://github.com/Ca11back/scf-experiment.git
+git clone --single-branch --branch master https://github.com/Ca11back/scf-experiment.git
 mv scf-experiment/config.json JD_tencent_scf/
 mv scf-experiment/index.js JD_tencent_scf/
 mv scf-experiment/serverless.yml JD_tencent_scf/
