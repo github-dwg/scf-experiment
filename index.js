640e69fe403ac3db64fbac1d9b047a8ad6d8df29
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-09-21 22:16:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.main_handler = async (event, context, callback) => {
     let scripts = []
     const single_flag = event["Message"] != 'config'
     if (single_flag) {
-        console.log('单脚本触发方式(不读取配置文件),Msg:', event["Message"])
+        console.log('单脚本触发方式(不读取配置文件),触发参数:', event["Message"])
         scripts.push(event["Message"])
     } else {
         const now_hour = (new Date().getUTCHours() + 8) % 24
@@ -20,7 +20,7 @@ exports.main_handler = async (event, context, callback) => {
         }
         let config
         try {
-            config = JSON.parse(await readFileSync(config_file));
+            config = JSON.parse(await readFileSync(config_file))
         } catch (e) {
             console.error(`read config error:${e}`)
             return
@@ -28,6 +28,21 @@ exports.main_handler = async (event, context, callback) => {
         // console.debug(JSON.stringify(config))
         params = config['params']
         delete config['params']
+
+        const config_diy_file = 'config_diy.json'
+        try {
+            await accessSync('./' + config_diy_file, constants.F_OK)
+            console.log(`${config_diy_file} 存在`)
+            const config_diy = JSON.parse(await readFileSync(config_diy_file))
+            if (config_diy['params']) {
+                params = { ...params, ...config_diy['params'] }
+                delete config_diy['params']
+            }
+            config = { ...config, ...config_diy }
+        } catch (err) {
+            console.error(`${config_diy_file} 不存在`)
+        }
+        console.log("params:", params)
         for (let script in config) {
             // console.debug(`script:${script}`)
             const cron = config[script]
@@ -56,36 +71,69 @@ exports.main_handler = async (event, context, callback) => {
             return !flag
         })
     }
-    const { execFileSync } = require('child_process')
+    let execFileSync
     const min = 1000 * 60
     const param_names = ['timeout']
-    for (const script of scripts) {
-        console.log(`run script:${script}, please waitting for log`)
-        const name = './' + script + '.js'
-        const param_run = {}
-        if (!single_flag){
-            const param = params[script]
-            for (const param_name of param_names) {
-                if (param) {
-                    if (param[param_name]) {
-                        console.debug(`${script} has specific ${param_name}:${param[param_name]}`)
-                        param_run[param_name] = min * param[param_name]
-                    }
-                } else if (params['global'] && params['global'][param_name]) {
-                    console.debug(`${script} use global ${param_name}`)
-                    param_run[param_name] = min * params['global'][param_name]
-                } else {
-                    console.warn(`No global ${param_name}!`)
+    const is_async = (params['global'] && params['global']['exec'] == 'async')
+    console.log('当前是', is_async ? '异' : '同', '步执行')
+    if (is_async) {
+        console.log('异步执行不支持params参数!')['log', 'warn', 'error', 'debug', 'info'].forEach((methodName) => {
+            const originalMethod = console[methodName]
+            console[methodName] = (...args) => {
+                try {
+                    throw new Error()
+                } catch (error) {
+                    let stack = error
+                        .stack // Grabs the stack trace
+                        .split('\n')[2] // Grabs third line
+                        .split("/").slice(-1)[0] // Grabs  file name and line number
+                        .replace('.js', '')
+                    stack = `${stack.substring(0, stack.lastIndexOf(':'))}:`
+                    originalMethod.apply(
+                        console,
+                        [
+                            stack,
+                            ...args
+                        ]
+                    )
                 }
             }
-        }
-        try {
-            const result = await execFileSync(process.execPath, [name], param_run)
-            console.log(result.toString())
-            console.log(`${script} finished`)
-        } catch (e) {
-            console.error(`${script} ERROR:${e}`)
-            console.error(`stdout:${e.stdout}`)
+        })
+    } else {
+        execFileSync = require('child_process').execFileSync
+    }
+    for (const script of scripts) {
+        console.log(`run script:${script}`)
+        const name = './' + script + '.js'
+        if (is_async) {
+            require(name)
+        } else {
+            console.log('please waitting for log')
+            const param_run = {}
+            if (!single_flag) {
+                const param = params[script]
+                for (const param_name of param_names) {
+                    if (param) {
+                        if (param[param_name]) {
+                            console.debug(`${script} has specific ${param_name}:${param[param_name]}`)
+                            param_run[param_name] = min * param[param_name]
+                        }
+                    } else if (params['global'] && params['global'][param_name]) {
+                        console.debug(`${script} use global ${param_name}`)
+                        param_run[param_name] = min * params['global'][param_name]
+                    } else {
+                        console.warn(`No global ${param_name}!`)
+                    }
+                }
+            }
+            try {
+                const result = await execFileSync(process.execPath, [name], param_run)
+                console.log(result.toString())
+                console.log(`${script} finished`)
+            } catch (e) {
+                console.error(`${script} ERROR:${e}`)
+                console.error(`stdout:${e.stdout}`)
+            }
         }
     }
 }
