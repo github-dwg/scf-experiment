0c2f206c385047d46e49aa59229bab075ee37103
Ca11back <24651341+Ca11back@users.noreply.github.com>
2021-09-27 18:33:20 +0800

    Update diy_jddj.sh

--- examples/diy_jddj.sh
+++ examples/diy_jddj.sh
@@ -3,3 +3,4 @@ echo 'DIY:Adding JDDJ'
 git clone --single-branch --branch main https://github.com/passerby-b/JDDJ.git
 rm -rf JDDJ/jddj_getck.js
 mv JDDJ/jddj* JD_tencent_scf/
+mv JDDJ/root.json JD_tencent_scf/
