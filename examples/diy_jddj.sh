0ae2c5229055a0dcd6795664fde2f8b2b77a02b3
github-dwg <90589433+github-dwg@users.noreply.github.com>
2021-10-06 13:54:09 +0800

    Update diy_jddj.sh

--- examples/diy_jddj.sh
+++ examples/diy_jddj.sh
@@ -2,5 +2,5 @@
 echo 'DIY:Adding JDDJ'
 git clone --single-branch --branch main https://github.com/passerby-b/JDDJ.git
 rm -rf JDDJ/jddj_getck.js
-mv JDDJ/jddj* JD_tencent_scf/
-mv JDDJ/root.json JD_tencent_scf/
+mv JDDJ/jddj* jd_dwg/
+mv JDDJ/root.json jd_dwg/
