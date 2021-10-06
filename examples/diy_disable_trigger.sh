9788faeb4be81e5bcba94185120297dcda51cc62
github-dwg <90589433+github-dwg@users.noreply.github.com>
2021-10-06 13:59:02 +0800

    Update diy_disable_trigger.sh

--- examples/diy_disable_trigger.sh
+++ examples/diy_disable_trigger.sh
@@ -1,4 +1,4 @@
 # 数字为行号,切换触发器为disable
-sed -i '37s/\(enable: \)true/\1false/g' JD_tencent_scf/serverless.yml
-sed -i '43s/\(enable: \)true/\1false/g' JD_tencent_scf/serverless.yml
-sed -i '49s/\(enable: \)true/\1false/g' JD_tencent_scf/serverless.yml
+sed -i '37s/\(enable: \)true/\1false/g' jd_dwg/serverless.yml
+sed -i '43s/\(enable: \)true/\1false/g' jd_dwg/serverless.yml
+sed -i '49s/\(enable: \)true/\1false/g' jd_dwg/serverless.yml
